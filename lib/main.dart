ae1ad712fe208d6de9aa1299e5684ef6a71e80f9
Houssam-Eddine <94980688+HoussamEddineLamzoudi@users.noreply.github.com>
2023-03-06 16:12:53 +0100

    add customize color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,23 @@ void main() {
   runApp(const MyApp());
 }
 
+//customize color
+MaterialColor simplonColor = const MaterialColor(
+  0xFFCE0033, // Primary color value
+  <int, Color>{
+    50: Color.fromRGBO(206, 0, 51, 0.1),
+    100: Color.fromRGBO(206, 0, 51, 0.2),
+    200: Color.fromRGBO(206, 0, 51, 0.3),
+    300: Color.fromRGBO(206, 0, 51, 0.4),
+    400: Color.fromRGBO(206, 0, 51, 0.5),
+    500: Color.fromRGBO(206, 0, 51, 0.6),
+    600: Color.fromRGBO(206, 0, 51, 0.7),
+    700: Color.fromRGBO(206, 0, 51, 0.8),
+    800: Color.fromRGBO(206, 0, 51, 0.9),
+    900: Color.fromRGBO(206, 0, 51, 1.0),
+  },
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
